5e99c8c8b9d883d452aa2662c50e46af51004209
Gabriele Canepa <gabriele@canepa.io>
2019-01-22 16:15:25 +0000

    Upload solution code

---
+++ scraper.rb
@@ -0,0 +1,32 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry-byebug'
+
+BASE_URL = 'http://www.imdb.com'
+
+def fetch_movies_url
+  html_doc = Nokogiri::HTML(open("#{BASE_URL}/chart/top").read)
+  html_doc.search('.titleColumn a').take(5).map do |link|
+    movie_id = link.attributes['href'].value.match(/\/title\/(?<id>\w{9})/)[:id]
+    "#{BASE_URL}/title/#{movie_id}"
+  end
+end
+
+def scrape_movie(movie_url)
+  html_doc = Nokogiri::HTML(open(movie_url).read)
+  title_match = html_doc.at('.title_wrapper h1').text.strip.match(/(?<title>.*)[[:space:]]\((?<year>\d{4})\)/)
+
+  title = title_match[:title]
+  year = title_match[:year].to_i
+  storyline = html_doc.at('.summary_text').text.strip
+  director = html_doc.at('.credit_summary_item').text.gsub(/Director:/, '').strip
+  cast = html_doc.search('.cast_list tr td:nth-child(2)').take(3).map { |element| element.text.strip }
+
+  {
+    cast: cast,
+    director: director,
+    storyline: storyline,
+    title: title,
+    year: year
+  }
+end
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,9 +1,27 @@
 require_relative '../scraper'
 
 describe '#fetch_movies_url' do
-  # TODO: return an array with the top 5 movies IMDB urls
+  it 'returns an array of movies' do
+    expected = [
+      'http://www.imdb.com/title/tt0111161',
+      'http://www.imdb.com/title/tt0068646',
+      'http://www.imdb.com/title/tt0071562',
+      'http://www.imdb.com/title/tt0468569',
+      'http://www.imdb.com/title/tt0050083'
+    ]
+    expect(fetch_movies_url).to eq(expected)
+  end
 end
 
 describe '#scrape_movie' do
-  # TODO: return an hash with the correct movie informations
+  it 'returns an hash with the correct informations' do
+    expected = {
+      cast: [ 'Christian Bale', 'Heath Ledger', 'Aaron Eckhart' ],
+      director: 'Christopher Nolan',
+      storyline: 'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+      title: 'The Dark Knight',
+      year: 2008
+    }
+    expect(scrape_movie('http://www.imdb.com/title/tt0468569')).to eq(expected)
+  end
 end
