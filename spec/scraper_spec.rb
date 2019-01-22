49eefd9a5a9ed2cb8629245dbbb42a2c8335e7dd
Gabriele Canepa <gabriele@canepa.io>
2019-01-22 12:38:35 +0000

    Upload livecode code

---
+++ spec/scraper_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../scraper'
+
+describe '#fetch_movies_url' do
+  # TODO: return an array with the top 5 movies IMDB urls
+end
+
+describe '#scrape_movie' do
+  # TODO: return an hash with the correct movie informations
+end
